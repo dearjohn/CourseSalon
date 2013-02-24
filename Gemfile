ef51ad5bd5dbdb341941433ffceedde5ee7f8400
john <mail2johnwong@gmail.com>
2013-02-24 12:57:39 +0800

    Start CourseSalon. keep shipping.

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+gem 'jquery-rails', '2.0.2'
+
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
+group :development do
+  gem 'sqlite3', '1.3.5'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseSalon::Application.config.secret_token = '3848ec3b1c661ae97eb5db503d02fc2b212c6d2bd4288b586bb033304db0977163883dd94b2634e9e33c6c9e899436291a83a9efb7edbd6954065cd47a811249'
