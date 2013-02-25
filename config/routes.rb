e710789b600ad25ad2c0ac605009b5daa849d728
john <mail2johnwong@gmail.com>
2013-02-25 22:09:42 +0800

    finish static pages: home & about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 CourseSalon::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/about"
+  root :to => 'static_pages#home'
+  
+  match '/about', to: 'static_pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,6 @@ CourseSalon::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
