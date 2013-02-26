b9c5646a1f75173139a0e132cd3564128931a1ab
john <mail2johnwong@gmail.com>
2013-02-26 23:21:56 +0800

    add password and validate

--- Gemfile
+++ Gemfile
@@ -2,9 +2,13 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 
+#使用bootstrap作为前端工具
 gem 'bootstrap-sass', '2.3'
 
 gem 'jquery-rails', '2.0.2'
+
+#用来处理用户密码
+gem 'bcrypt-ruby', '3.0.1'
 
 group :assets do
   gem 'sass-rails',   '3.2.5'
@@ -16,6 +20,7 @@ group :development do
   gem 'sqlite3', '1.3.5'
 end
 
+#部署在heroku时需要使用pg数据库
 group :production do
   gem 'pg', '0.12.2'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130226150041) do
+ActiveRecord::Schema.define(:version => 20130226150932) do
 
   create_table "users", :force => true do |t|
     t.integer  "role"
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
