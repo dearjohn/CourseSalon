fbe0c750bb5ad7ff26af26ad6faa1407590aad7f
john <mail2johnwong@gmail.com>
2013-02-26 22:56:41 +0800

    add email-format validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :role
 
   validates :role, presence: true
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_FORMAT }
+
 end
