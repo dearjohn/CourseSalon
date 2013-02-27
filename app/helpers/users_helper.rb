a66fc72d9c0886ae936f9ef21e8ec4878af86907
john <mail2johnwong@gmail.com>
2013-02-27 22:43:13 +0800

    add avatar to user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+	def gravatar_for(current_user)
+		gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
+		gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: current_user.name)
+	end
 end
