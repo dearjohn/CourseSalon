7acb80b954d793c779a1d96568588a5da3b700f1
john <mail2johnwong@gmail.com>
2013-03-06 22:52:27 +0800

    解决了从数据库层面对 email唯一性的验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   validates :role, presence: true#这一块仍未修复, :exclusion => { :in => %w(0 1),
-   :message => "role must be 0 or 1(0 for teacher / 1 for student)" }  
+  # :message => "role must be 0 or 1(0 for teacher / 1 for student)" }  
 
   #利用正则表达式来验证email的格式
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #这里要验证唯一性的时候，光是用uniqueness处理是不行的: a new-> b new -> a.save -> b.save
   #在这里从数据库层面来处理，加入唯一性索引
-  validates :email, presence: true, format: { with: EMAIL_FORMAT }
+  validates :email, presence: true, format: { with: EMAIL_FORMAT },uniqueness: { case_sensitive: false }
 
   #存入数据库之前做小写处理
   before_save { |user| user.email = email.downcase }
