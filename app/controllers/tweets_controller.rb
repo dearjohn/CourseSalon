2a9cf6c03eb1eb1a70e89cb1a118ba481efedf60
john <mail2johnwong@gmail.com>
2013-04-18 01:16:34 +0800

    Delete Tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,21 @@
 class TweetsController < ApplicationController
   before_filter :authorize, only: [:create, :destroy]
-
+  
   def create
     @tweet = current_user.tweets.build(params[:tweet])
     if @tweet.save
       flash[:success] = "New tweet!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
+  #Tutorial中说这种删除方法不安全？
   def destroy
+    current_user.tweets.find_by_id(params[:id]).destroy
+    redirect_to root_url
   end
+
 end
