9af62f8b070e644d9c8904a7941b8b9e155c77e1
john <mail2johnwong@gmail.com>
2013-04-19 11:37:26 +0800

    teacher can create courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,25 @@
 class CoursesController < ApplicationController
   before_filter :authorize
-
+  
   def new
-    @course = Course.new
+    @course = current_user.courses.build
   end
 
+  def create
+    @course = current_user.courses.build(params[:course])
+    if @course.save
+      flash[:success] = "New course!"
+      render 'index'
+    else
+      flash[:fail] = "fail!"    	
+      render 'new'
+    end
+  end
+
+  def show
+  	@course = Course.find(params[:id])		
+  end
+  
   def index
-    
   end
 end
